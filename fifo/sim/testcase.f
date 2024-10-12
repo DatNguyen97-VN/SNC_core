ca7714ecfa9676f30dc825daf91a6998ccb6d715
1st-Third-Reich <thanhdat361997@gmail.com>
2024-10-12 22:49:16 +0700

    add new testcase

--- fifo/sim/testcase.f
+++ fifo/sim/testcase.f
@@ -1,1 +1,7 @@
 ../tests/syn_fifo_tc_01.sv
+../tests/syn_fifo_tc_02.sv
+../tests/syn_fifo_tc_03.sv
+../tests/syn_fifo_tc_04.sv
+../tests/syn_fifo_tc_05.sv
+../tests/syn_fifo_tc_06.sv
+../tests/syn_fifo_tc_07.sv
